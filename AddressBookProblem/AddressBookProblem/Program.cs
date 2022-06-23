e1956a4a179a7106e71d0ce45935b7a7c954ea67
mohan3388 <sahumohan338@gmail.com>
2022-06-23 00:21:46 -0700

    [mohan]add: create database connection

---
+++ AddressBookProblem/AddressBookProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AddressBookProblem;
+
+namespace AddressBookProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+            addressBook.SetConnection();
+        }
+    }
+}
